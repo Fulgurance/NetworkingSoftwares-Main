03e23688d7b5f95ab50208f05554ad644cfb44a7
Fulgurance <zohran.londais@gmail.com>
2024-06-27 13:18:40 +0100

    Updated installers

--- Discord/0.0.58/0.0.58.cr
+++ Discord/0.0.58/0.0.58.cr
@@ -7,4 +7,11 @@ class Target < ISM::Software
                     newPath: "#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}/usr")
     end
 
+    def install
+        super
+
+        runChownCommand("root:root /usr/share/discord/chrome-sandbox")
+        runChmodCommand("4755 /usr/share/discord/chrome-sandbox")
+    end
+
 end
