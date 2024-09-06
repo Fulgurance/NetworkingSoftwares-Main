1dbd45480a85d654568b0a063fa21b1ae3056405
Fulgurance <zohran.londais@gmail.com>
2024-09-06 13:53:14 +0100

    Added wireshark

---
+++ Wireshark/4.0.7/4.0.7.cr
@@ -0,0 +1,48 @@
+class Target < ISM::Software
+
+    def prepare
+        @buildDirectory = true
+        super
+    end
+    
+    def configure
+        super
+
+        runCmakeCommand(arguments:  "-DCMAKE_INSTALL_PREFIX=/usr                            \
+                                    -DCMAKE_BUILD_TYPE=Release                              \
+                                    -DCMAKE_INSTALL_DOCDIR=/usr/share/doc/wireshark-4.0.7   \
+                                    -G Ninja                                                \
+                                    ..",
+                        path:       buildDirectoryPath)
+    end
+    
+    def build
+        super
+
+        runNinjaCommand(path: buildDirectoryPath)
+    end
+
+    def prepareInstallation
+        super
+
+        runNinjaCommand(arguments:      "install",
+                        path:           buildDirectoryPath,
+                        environment:    {"DESTDIR" => "#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}"})
+    end
+
+    def install
+        super
+
+        runChownCommand("root:wireshark /usr/bin/tshark")
+        runChownCommand("root:wireshark /usr/bin/dumpcap")
+        runChmodCommand("6550 /usr/bin/tshark")
+        runChmodCommand("6550 /usr/bin/dumpcap")
+    end
+
+    def showInformations
+        super
+
+        showInfo("After the installation, if you wish a user able to use wireshark, add it in the wireshark system group")
+    end
+
+end
