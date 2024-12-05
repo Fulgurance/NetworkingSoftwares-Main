f8cac8e8d6f5136604b58a3a2e7a4f2b476002ef
Fulgurance <zohran.londais@gmail.com>
2024-12-05 14:36:42 +0000

    Updated installer

--- Slack/4.38.125/4.38.125.cr
+++ Slack/4.38.125/4.38.125.cr
@@ -1,21 +1,2 @@
-class Target < ISM::Software
-
-    def prepareInstallation
-        super
-
-        moveFile(   path:       "#{buildDirectoryPath}/etc",
-                    newPath:    "#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}/etc")
-
-        moveFile(   path:       "#{buildDirectoryPath}/usr",
-                    newPath:    "#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}/usr")
-    end
-
-    def install
-        super
-
-        runChownCommand("-R root:root /usr/lib/slack")
-        runChownCommand("root:root /usr/share/applications/slack.desktop")
-        runChmodCommand("4755 /usr/lib/slack/chrome-sandbox")
-    end
-
+class Target < ISM::PackagedSoftware
 end
