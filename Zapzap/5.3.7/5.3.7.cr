4a1a5c5538c223e2143c00bb94014684c7a28544
Fulgurance <zohran.londais@gmail.com>
2024-12-14 21:29:49 +0000

    Updated installation process

--- Zapzap/5.3.7/5.3.7.cr
+++ Zapzap/5.3.7/5.3.7.cr
@@ -17,6 +17,13 @@ class Target < ISM::Software
 
         moveFile(   path:       "#{buildDirectoryPath}/share",
                     newPath:    "#{builtSoftwareDirectoryPath}/#{Ism.settings.rootPath}/usr/share")
+
+        makeDirectory("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/profile.d")
+
+        pythonData = <<-CODE
+        pathappend /usr/lib/python#{majorVersion}.#{minorVersion}/site-packages/zapzap-5.3-py#{majorVersion}.#{minorVersion}.egg PYTHONPATH
+        CODE
+        fileUpdateData("#{builtSoftwareDirectoryPath}#{Ism.settings.rootPath}etc/profile.d/python.sh",pythonData)
     end
 
 end
