84ec5a88d89e63a81054e16597904ec7a9fdf8d7
Fulgurance <zohran.londais@gmail.com>
2025-04-19 00:04:56 +0100

    Added deployement phase

--- Wpa-Supplicant/2.11.0/2.11.0.cr
+++ Wpa-Supplicant/2.11.0/2.11.0.cr
@@ -88,4 +88,12 @@ class Target < ISM::Software
         end
     end
 
+    def deploy
+        super
+
+        if Ism.settings.autoDeployServices
+            runRcUpdateCommand("add wpa-supplicant default")
+        end
+    end
+
 end
